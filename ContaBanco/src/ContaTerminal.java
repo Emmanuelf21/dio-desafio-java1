10a7dbcd420245d32ee13cb7addcbc531be412b8
Emmanuel Franco <emmanuelfranco2001@gmail.com>
2024-05-02 14:52:20 -0300

    conta bancária básico

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+import javax.sound.midi.Soundbank;
+
+import java.util.Locale;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Por favor, digite o número da conta: ");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o número da Agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, Digite seu nome: ");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o saldo: ");
+        double saldo = scanner.nextDouble();
+        
+        System.out.println("Olá, "+nomeCliente+" obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+numero+" e seu saldo "+saldo+" já está disponível para saque");
+    }
+}
